f4f316a310b2876f3866074ebec99848e8b4373b
RyannKim327 <mpoprev2@email.com>
2023-09-09 01:30:44 +0800

    9/8/2023 7:30:43 PM

--- ProjectBase/main.py
+++ ProjectBase/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from database.database import *
+import json
 
 db = Database()
 app = Flask(__name__)
@@ -7,5 +8,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 	return render_template("index.html")
-	# data = db.getUsers()
-	# return f"<h1>{data}</h1>"
+
+@app.route("/api/user")
+def api_user():
+	return json.dumps({
+		"users": "sample"
+	})
