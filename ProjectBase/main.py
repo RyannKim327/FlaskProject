45ceeff2de6b3e7c226760a67553e81f65cf4c88
RyannKim327 <mpoprev2@email.com>
2023-09-09 00:12:39 +0800

    9/8/2023 6:12:38 PM

--- ProjectBase/main.py
+++ ProjectBase/main.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "<h1>Hello World</h1>"	
+	return "<h1>Hello World</h1>"
