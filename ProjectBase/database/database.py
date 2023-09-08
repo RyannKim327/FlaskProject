7827650dddee3bdd930b2dfed56fce8002690903
RyannKim327 <mpoprev2@email.com>
2023-09-09 00:36:09 +0800

    9/8/2023 6:36:09 PM

--- ProjectBase/database/database.py
+++ ProjectBase/database/database.py
@@ -3,4 +3,6 @@ import sqlite3
 con = sqlite3.connect("db.sqlite")
 cur = con.cursor()
 
-cur.execute("CREATE TABLE IF NOT EXISTS users ('ID')")
+cur.execute("CREATE TABLE IF NOT EXISTS users ('ID', 'usn', 'pass')")
+
+con.commit()
--- ProjectBase/main.py
+++ ProjectBase/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from database.database import *
 app = Flask(__name__)
 
 @app.route("/")
