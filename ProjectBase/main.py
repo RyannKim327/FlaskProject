f63988cad21cb215ffc9c33ea322cdbda1f72969
RyannKim327 <mpoprev2@email.com>
2023-09-09 01:34:56 +0800

    9/8/2023 7:34:55 PM

--- ProjectBase/main.py
+++ ProjectBase/main.py
@@ -1,16 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from database.database import *
-import json
 
 db = Database()
 app = Flask(__name__)
 
+# Frontend connection
 @app.route("/")
 def index():
 	return render_template("index.html")
 
-@app.route("/api/user")
+# Api Integration
+@app.route("/api/users")
 def api_user():
-	return json.dumps({
+	return jsonify({
 		"users": "sample"
 	})
