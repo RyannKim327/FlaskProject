4fd0773b949e6b76b2a9a99010e37f9ac7eff9b7
RyannKim327 <mpoprev2@email.com>
2023-09-09 00:40:21 +0800

    9/8/2023 6:40:20 PM

--- ProjectBase/database/database.py
+++ ProjectBase/database/database.py
@@ -3,6 +3,15 @@ import sqlite3
 con = sqlite3.connect("db.sqlite")
 cur = con.cursor()
 
-cur.execute("CREATE TABLE IF NOT EXISTS users ('ID', 'usn', 'pass')")
+cur.execute("""CREATE TABLE IF NOT EXISTS users (
+	'ID' INTEGER AUTO INCREMENT, 
+	'usn' TEXT, 
+	'pass' TEXT
+)""")
 
 con.commit()
+
+# User
+def addUser(name: str, password: str):
+	cur.execute()
+	con.commit()
