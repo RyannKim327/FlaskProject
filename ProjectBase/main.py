4f8ae460f826a9b45f06e29111d9cc529ad4e6cd
RyannKim327 <mpoprev2@email.com>
2023-09-12 03:43:35 +0800

    9/12/2023 3:43:35 AM [Auto Commit]

--- ProjectBase/main.py
+++ ProjectBase/main.py
@@ -47,17 +47,18 @@ def logged():
 	if request.cookies.get("user") != "":
 		db  = Database()
 		user = db.getCurrentUser(request.cookies)
-		if len(user) > 0:
-			return jsonify({
-				"status": True,
-				"data": "Logged in"
-			})
-		else:
-			return jsonify({
-				"status": False,
-				"data": "Invalids"
-			})
-	return jsonify({
-		"status": False,
-		"data": "Invalids"
-	})
+	# 	if len(user) > 0:
+	# 		return jsonify({
+	# 			"status": True,
+	# 			"data": "Logged in"
+	# 		})
+	# 	else:
+	# 		return jsonify({
+	# 			"status": False,
+	# 			"data": "Invalids"
+	# 		})
+	# return jsonify({
+	# 	"status": False,
+	# 	"data": "Invalids"
+	# })
+	return user
